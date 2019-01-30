aedb4e80cef6edebd54bca98cf23521373ca315f
Andrew Mashukov <a.mashukoff@gmail.com>
2019-01-30 11:27:55 +0300

    Update Blob.php
    Fixed blob encoding

--- src/Wrapper/Type/Blob.php
+++ src/Wrapper/Type/Blob.php
@@ -6,6 +6,6 @@ class Blob implements CustomTypeInterface
 {
     public function convert(string $name, string $alias): string
     {
-        return "CONVERT({$alias}.{$name} USING utf8)";
+        return "CONVERT({$alias}.{$name} USING utf8mb4)";
     }
 }
